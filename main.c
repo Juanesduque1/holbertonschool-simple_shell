5e2776902b3961231abee6015ed24c590d736253
YuranyUlchur <ulchuryurani80@gmail.com>
2022-12-22 15:21:19 -0500

    add the return status

--- main.c
+++ main.c
@@ -17,15 +17,17 @@ int main(void)
 	{
 		if (isatty(STDIN_FILENO) != 0)
 			printf("$ ");
-		buff_len = getline(&buff, &len, stdin); /* Obtaining user's command */
+		buff_len = getline(&buff, &len, stdin);				/* Obtaining user's command */
 		if (buff_len == -1 || _strcmp("exit\n", buff) == 0) /*Check if its "exit" */
 		{
 			free(buff);
 			break; }
 		buff[buff_len - 1] = '\0';
 		if (_strcmp("env", buff) == 0) /* Check if its env */
+		{
 			_env();
-		args = _divstring(buff, " "); /* Divide input and allocate it in args */
+			continue; }
+		args = _divstring(buff, " ");  /* Divide input and allocate it in args */
 		args[0] = check_path(args[0]); /* Check if command is valid */
 		if (args[0] != NULL)
 		{
@@ -33,18 +35,16 @@ int main(void)
 			if (p_id == 0)
 			{
 				if (execve(args[0], args, environ) == -1) /* Execute valid command */
-					perror("Error");
-			}
+					perror("Error"); }
 			else
 			{
-				wait(&status); /* Wait until child process exits */
-				if (WIFEXITED(status)) /* True if child process terminates normally */
-					status = WEXITSTATUS(status); /* Returns exit status specified */
-			}
+				wait(&status);					  /* Wait until child process exits */
+				if (WIFEXITED(status))			  /* True if child process terminates normally */
+					status = WEXITSTATUS(status); } /* Returns exit status specified */
 		}
 		else
 			perror("Error"); /* Displays error's description if not forked */
 		free(args);
 	}
-	return (0);
+	return (status);
 }
